cd64b24960bbda4ec29c3aec685f80b13f2665b5
jma <masson.julien@outlook.be>
2026-02-24 08:06:13 +0100

    Init branch 17.0

--- dasolo_shopify_connector/__manifest__.py
+++ dasolo_shopify_connector/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Dasolo Shopify Connector',
-    'version': '19.0.1.0.0',
+    'version': '17.0.1.0.0',
     'category': 'Sales',
     'summary': 'Connect your Shopify store with Odoo seamlessly',
     'description': """
